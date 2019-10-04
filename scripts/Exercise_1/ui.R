177c028053bf74db289f7c03bb541409de64756a
Konstantin Gavras <konstantin@gavras.de>
2019-10-04 15:43:27 +0200

    deployment test

--- scripts/Exercise_1/ui.R
+++ scripts/Exercise_1/ui.R
@@ -1,7 +1,29 @@
 library(shiny)
 #The most simple structure of the UI:
 ui <- fluidPage(
-  "Hello World",
-  h1("This is a Title"),
-  titlePanel("This is an actual Title")
+  titlePanel("This is an actual Title"),
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        "buttons",
+        "Push a button",
+        choices = c("One", "Two", "Three"),
+        selected = "One"
+      ),
+      selectInput(
+        "selector",
+        "Select a color",
+        choices = c("Blue", "Green", "Yellow")
+      ),
+      sliderInput(
+        "slider",
+        "Pick a range",
+        min = 0,
+        max = 100,
+        value = c(10,30),
+        pre = "€"
+      )
+    ),
+    mainPanel()
+  )
 )
